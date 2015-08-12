920fb3e11aec19c62835f27e95960ce835f29d49
David Dooling <dooling@gmail.com>
2015-08-12 09:08:00 -0500

    ensure yaml files end in a newline

--- src/main/scala/com/monsanto/arch/cloudformation/model/simple/Yaml.scala
+++ src/main/scala/com/monsanto/arch/cloudformation/model/simple/Yaml.scala
@@ -44,7 +44,7 @@ object Yaml {
 
     def yaml(args: Any*): String = {
       sc.checkLengths(args)
-      build(sc.parts, args).mkString.trim
+      build(sc.parts, args).mkString.trim + "\n"
     }
   }
 

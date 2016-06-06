85cfc94ab10d6faddfcc5ac83685723c80e8938a
Adam Nelson <anelson@apocryph.org>
2016-06-06 13:44:29 -0400

    [#99] Implement support for AWS::CloudFormation::Stack so templates can invoke other nested CF templates

--- src/main/scala/com/monsanto/arch/cloudformation/model/resource/CloudFormation.scala
+++ src/main/scala/com/monsanto/arch/cloudformation/model/resource/CloudFormation.scala
@@ -1,6 +1,6 @@
 package com.monsanto.arch.cloudformation.model.resource
 
-import com.monsanto.arch.cloudformation.model.{Token, ConditionRef}
+import com.monsanto.arch.cloudformation.model._
 import spray.json.JsonFormat
 
 /**
@@ -38,3 +38,23 @@ object `AWS::CloudFormation::WaitCondition` {
   implicit val format: JsonFormat[`AWS::CloudFormation::WaitCondition`] = jsonFormat6(`AWS::CloudFormation::WaitCondition`.apply)
 }
 
+case class `AWS::CloudFormation::Stack`(name: String,
+                                        TemplateURL: Token[String],
+                                        TimeoutInMinutes: Option[Int] = None,
+                                        Parameters: Option[Map[String, String]] = None,
+                                        override val Condition: Option[ConditionRef] = None)
+    extends Resource[`AWS::CloudFormation::Stack`]
+    with HasArn {
+
+  override def arn = `Fn::GetAtt`(Seq(name, "Arn"))
+
+  def when(newCondition: Option[ConditionRef] = Condition) = copy(Condition = newCondition)
+}
+
+object `AWS::CloudFormation::Stack` {
+
+  import spray.json.DefaultJsonProtocol._
+
+  implicit val format: JsonFormat[`AWS::CloudFormation::Stack`] = jsonFormat5(
+      `AWS::CloudFormation::Stack`.apply)
+}

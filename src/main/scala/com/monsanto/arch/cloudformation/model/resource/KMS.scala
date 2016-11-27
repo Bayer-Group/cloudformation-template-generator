47d48e4a63b11581644e8e51979864130d1c532f
Tyler Southwick <tyler@northfuse.net>
2016-11-26 21:57:15 -0800

    add support for KMS

---
+++ src/main/scala/com/monsanto/arch/cloudformation/model/resource/KMS.scala
@@ -0,0 +1,37 @@
+package com.monsanto.arch.cloudformation.model.resource
+
+import com.monsanto.arch.cloudformation.model._
+import spray.json.DefaultJsonProtocol._
+import spray.json.RootJsonFormat
+
+case class `AWS::KMS::Alias`(
+                            name : String,
+                            AliasName : Token[String],
+                            TargetKeyId : Token[String],
+                            override val Condition : Option[ConditionRef] = None
+                            ) extends Resource[`AWS::KMS::Alias`] {
+  override def when(newCondition: Option[ConditionRef]): `AWS::KMS::Alias` = copy(Condition = newCondition)
+}
+object `AWS::KMS::Alias` {
+
+  implicit val format : RootJsonFormat[`AWS::KMS::Alias`] = jsonFormat4(`AWS::KMS::Alias`.apply)
+}
+
+case class `AWS::KMS::Key`(
+                          name : String,
+                          Description : Option[Token[String]] = None,
+                          Enabled : Option[Token[Boolean]] = None,
+                          EnableKeyRotation : Option[Token[Boolean]] = None,
+                          KeyPolicy : PolicyDocument,
+                          override val Condition: Option[ConditionRef] = None
+                          ) extends Resource[`AWS::KMS::Key`] with HasArn {
+  override def when(newCondition: Option[ConditionRef]): `AWS::KMS::Key` = copy(Condition = newCondition)
+
+  override def arn: Token[String] = `Fn::GetAtt`(Seq(name, "Arn"))
+}
+
+object `AWS::KMS::Key` {
+
+  implicit val format : RootJsonFormat[`AWS::KMS::Key`] = jsonFormat6(`AWS::KMS::Key`.apply)
+}
+

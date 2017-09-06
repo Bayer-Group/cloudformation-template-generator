8605cc8fa334244a2067affd6b68b2258fa4f4ad
Sean McLaughlin <seanmcl@gmail.com>
2017-09-06 14:49:37 -0700

    Add `DependsOn` to `AWS::Lambda::EventSourceMapping`

--- src/main/scala/com/monsanto/arch/cloudformation/model/resource/Lambda.scala
+++ src/main/scala/com/monsanto/arch/cloudformation/model/resource/Lambda.scala
@@ -98,6 +98,7 @@ case class `AWS::Lambda::EventSourceMapping`(
                                               EventSourceArn: Token[String],
                                               FunctionName: Token[ResourceRef[`AWS::Lambda::Function`]],
                                               StartingPosition: Token[String],
+                                              override val DependsOn: Option[Seq[String]] = None,
                                               override val Condition: Option[ConditionRef] = None
                                               ) extends Resource[`AWS::Lambda::EventSourceMapping`] {
 
@@ -105,7 +106,7 @@ case class `AWS::Lambda::EventSourceMapping`(
 }
 
 object `AWS::Lambda::EventSourceMapping` {
-  implicit val format: JsonFormat[`AWS::Lambda::EventSourceMapping`] = jsonFormat7(`AWS::Lambda::EventSourceMapping`.apply)
+  implicit val format: JsonFormat[`AWS::Lambda::EventSourceMapping`] = jsonFormat8(`AWS::Lambda::EventSourceMapping`.apply)
 }
 
 case class `AWS::Lambda::Version`(name: String,

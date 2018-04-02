03f726e980b810977715fab8fc3d3f2dfb1fc0d5
Nguyen Hong Phuc (フック） <nguyen_phuc@cyberagent.co.jp>
2018-04-03 02:14:33 +0900

    add some image-ids for asia regions

--- src/main/scala/com/monsanto/arch/cloudformation/model.scala
+++ src/main/scala/com/monsanto/arch/cloudformation/model.scala
@@ -64,10 +64,13 @@ import scala.language.implicitConversions
   *      val amazonLinuxAMIMapping = Mapping[AMIId](
   *        "AmazonLinuxAMI",
   *        Map(
-  *          "us-east-1" -> Map("AMI" -> AMIId("ami-1ecae776")),
-  *          "us-west-1" -> Map("AMI" -> AMIId("ami-d114f295")),
-  *          "us-west-2" -> Map("AMI" -> AMIId("ami-e7527ed7")),
-  *          "eu-west-1" -> Map("AMI" -> AMIId("ami-a10897d6"))
+  *          "us-east-1"      -> Map("AMI" -> AMIId("ami-1ecae776")),
+  *          "us-west-1"      -> Map("AMI" -> AMIId("ami-d114f295")),
+  *          "us-west-2"      -> Map("AMI" -> AMIId("ami-e7527ed7")),
+  *          "eu-west-1"      -> Map("AMI" -> AMIId("ami-a10897d6")),
+  *          "ap-southeast-1" -> Map("AMI" -> AMIId("ami-68d8e93a")),
+  *          "ap-southeast-2" -> Map("AMI" -> AMIId("ami-fd9cecc7")),
+  *          "ap-northeast-1" -> Map("AMI" -> AMIId("ami-cbf90ecb"))
   *        )
   *      )
   *      val simpleMappings = Seq(amazonLinuxAMIMapping)
